cab8b67ee9e56a68def2a8b9da6751817c0ee98b
Bouabdallah khaled <killer99dz@gmail.com>
2023-12-18 15:17:21 +0100

    add logger, helper functions, generate possible complete

--- program.py
+++ program.py
@@ -1,9 +1,21 @@
-import sys
-import os.path
 import argparse
+import logging
+import datetime
 
-def load_AF(path):
-    pass
+
+# Set up the logging configuration with a FileHandler
+#log_filename = f"log_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+log_filename = "log.txt"
+log_handler = logging.FileHandler(log_filename, mode='w')
+log_handler.setLevel(logging.DEBUG)  # Set the level to the lowest (DEBUG)
+log_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+# Create a logger and add the FileHandler to it
+logger = logging.getLogger()
+logger.addHandler(log_handler)
+logger.setLevel(logging.DEBUG)  # Set the logger level to the lowest (DEBUG)
+
+
+# Example usage
 
 class AF():
     
@@ -19,6 +31,9 @@ class AF():
                 elif line.startswith("att"):
                     attack = [line[4:].split(",")[0],line[4:].split(",")[1].split(")")[0]]
                     self.attacks.append(attack)
+        logger.info(f'list of arguments: {self.args}') 
+        logger.info(f'list of attacks: {self.attacks}')             
+        
     
     
     def VE_CO(self) -> bool:
@@ -39,6 +54,50 @@ class AF():
     def DS_ST(self) -> bool:
         pass
     
+    def _get_attacks_by_(self,s) -> list: # get list of attacks done by set s (list of lists)
+        attacks = []    
+        for attack in self.attacks:
+            if attack[0] in s:
+                attacks.append(attack)
+        return attacks
+    
+    def _is_attacked_by_(self,s,z) -> bool: # return True if set 's' attack is attacked by set 'z'
+        attacks = self._get_attacks_by_(z)
+        for a in s:   
+            for attack in attacks :
+                if attack[1] == a:
+                    return True
+        return False
+    
+    def _get_attacked_by_(self,s,z) -> list: # get subset of s which is attacked by set z
+        attacked = []
+        for a in s:
+            if self._is_attacked_by_([a],z):
+                attacked.append(a)
+        return attacked
+    
+ 
+    
+  
+    def generate_possible_complete(self) -> list:
+        result = []
+        # find nodes that are not attacked 
+        attacked = self._get_attacked_by_(self.args,self.args)
+        not_attacked = set(self.args).difference(set(attacked))
+        result.append(not_attacked)
+        logger.info(f"not attacked at all: {not_attacked}")         
+        if not_attacked: # we found something 
+            while True:
+                new_attacked = self._get_attacked_by_(self.args,result[-1]) # argument that are attacked with our initial "grounded"
+                new_attackers = set(self.args).difference(set(new_attacked)) # remove their attacks 
+                new_attacked = self._get_attacked_by_(self.args,new_attackers) # find elements that are attacked
+                new_not_attacked = set(self.args).difference(set(new_attacked)) # find new set that are not attacked after 
+                logger.info(f"new possible complete: {new_not_attacked}")
+                if new_not_attacked in result:
+                    return result
+                result.append(new_not_attacked)
+        else: # we only have the empty set 
+            pass
 
     def is_admissible(this, E: list) -> bool:      #returns True if E is an admissable set
         for i in range(len(E)):                    #verify conflict-freeness
@@ -85,8 +144,7 @@ def main():
     args = parser.parse_args()
     # ##
     af = AF(path=args.f)
-    print(af.args)
-    print(af.attacks)
+    af.generate_possible_complete()
     #print(af.is_admissible(['a','c','d']))          #a little test..
     
     if args.p == "VE-CO":
@@ -109,3 +167,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    log_handler.close()
