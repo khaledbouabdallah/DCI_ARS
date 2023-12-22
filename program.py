77ab92ec2bfa3fa498cca2a499a7d4a5754eb31b
Ruslan <79635186+333nova@users.noreply.github.com>
2023-12-23 00:57:22 +0100

    Update program.py
    several updates

--- program.py
+++ program.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import numpy as np
 import datetime
 
 
@@ -36,136 +35,90 @@ class AF():
         logger.info(f'list of attacks: {self.attacks}')             
         
     
-    
+
+    #MAIN FUNCTIONS
+
     def VE_CO(this, E) -> bool:
-        list_co = this.generate_possible_complete()
+        list_co = this.generate_all_cf()
+        print(list_co)
+        res = []
+        for elem in list_co:                 #we check for each elem if is complete so that res is the list of all co
+            if this.is_complete(elem):
+                res.append(elem)         
 
-        for elem in list_co:                 #we check for each elem if is complete
-            if not this.is_complete(elem):
-                list_co.remove(elem)         #if not we remove it from the list
-
-        return True if set(E) in list_co else False      #return True if E in the list of complete extensions
-    
+        print(res)
+        return True if set(E) in res else False      
 
     def DC_CO(this, a) -> bool:
-        list_co = this.generate_possible_complete()
-
+        list_co = this.generate_all_cf()
+        res = []
         for elem in list_co:                 #we check for each elem if is complete
-            if not this.VE_CO(elem):
-                list_co.remove(elem)         #if not we remove it from the list
+            if this.VE_CO(elem):
+                res.append(elem)         
 
         for elem in list_co:                 #we check if a is in one co
             if a in elem:
                 return True
-        return False                         #if not we return False
-    
+        return False                        
 
     def DS_CO(this, a) -> bool:
-        list_co = this.generate_possible_complete()
-
+        list_co = this.generate_all_cf()
+        res = []
         for elem in list_co:                 #we check for each elem if is complete
-            if not this.VE_CO(elem):
-                list_co.remove(elem)         #if not we remove it from the list
+            if this.VE_CO(elem):
+                res.append(elem)         
 
         for elem in list_co:                 #we check if a is in each co
             if a not in elem:
                 return False
-        return True                          #if not we return True
+        return True                         
 
     
+
     def VE_ST(this, E) -> bool:
-        list_co = this.generate_possible_complete()
+        list_co = this.generate_all_cf()
+        print(list_co)
+        res = []
+        for elem in list_co:                 #we check for each elem if is stable so that res is the list of all st
+            if this.is_stable(elem):
+                res.append(elem)         
 
-        for elem in list_co:                 #we check for each elem if is stable
-            if not this.is_stable(elem):
-                list_co.remove(elem)         #if not we remove it from the list
-
-        return True if set(E) in list_co else False   #return True if E in the list of stable extensions
+        print(res)
+        return True if set(E) in res else False      
     
-
     def DC_ST(this, a) -> bool:
-        list_co = this.generate_possible_complete()
+        list_co = this.generate_all_cf()
+        res = []
+        for elem in list_co:                 #we check for each elem if is Stable
+            if this.VE_ST(elem):
+                res.append(elem)            
 
-        for elem in list_co:                 #we check for each elem if is complete
-            if not this.VE_ST(elem):
-                list_co.remove(elem)         #if not we remove it from the list
-
-        for elem in list_co:                 #we check if a is in one co
+        for elem in list_co:                 #we check if a is in one st
             if a in elem:
                 return True
-        return False                         #if not we return False
+        return False                         
 
-    
     def DS_ST(this, a) -> bool:
-        list_co = this.generate_possible_complete()
+        list_co = this.generate_all_cf()
+        res = []
+        for elem in list_co:                 
+            if this.VE_ST(elem):
+                res.append(elem)            
 
-        for elem in list_co:                 #we check for each elem if is complete
-            if not this.VE_ST(elem):
-                list_co.remove(elem)         #if not we remove it from the list
-
-        for elem in list_co:                 #we check if a is in each co
+        for elem in list_co:                 #we check if a is in each st
             if a not in elem:
                 return False
-        return True                          #if not we return True
+        return True                         
     
 
-
-    def _get_attacks_by_(self,s) -> set: # get list of attacks done by set s (list of lists)
-        attacks = [] 
-        for attack in self.attacks:
-            if attack[0] in s:
-                attacks.append(attack)
-        return attacks
-    
-    def _is_attacked_by_(self,s,z) -> bool: # return True if set 's' is attacked by set 'z'
-        attacks = self._get_attacks_by_(z)
-        for a in s:   
-            for attack in attacks :
-                if attack[1] == a:
-                    return True
-        return False
-    
-    def _get_attacked_by_(self,s,z) -> set: # get subset of 's' which is attacked by set 'z'
-        attacked = set()
-        for a in s:
-            if self._is_attacked_by_([a],z):
-                attacked.add(a)
-        return attacked
-    
-    def _get_not_attacked_by(self,s,z) -> set: # get subset of 's' which are not attacked by set 'z'
-        arguments = set(self.args)
-        return arguments.difference(self._get_attacked_by_(s,z))
-    
- 
-    
-  
-    def generate_possible_complete(self) -> list:
-        result = []
-        # find nodes that are not attacked by anyone
-        not_attacked =  self._get_not_attacked_by(self.args,self.args)
-        result.append(not_attacked)
-            
-        if not_attacked: # we found something 
-            logger.info(f"not attacked at all (grounded): {not_attacked}")     
-            while True:
-                new_attackers = self._get_not_attacked_by(self.args,result[-1]) # original set - set which are attacked by the grounded (first iteration example) #1
-                new_not_attacked = self._get_not_attacked_by(self.args,new_attackers) # set which are not attacked by #1 (defended by grounded (first iteration))
-                logger.info(f"new possible complete: {new_not_attacked}")
-                if new_not_attacked in result:
-                    return result
-                result.append(new_not_attacked)
-        else: # we only have the empty set 
-            logger.info(f"not attacked at all (grounded): empty set")            
-            pass
 
 
 
     def is_admissible(this, E: list) -> bool:      #returns True if E is an admissable set
-        for i in range(len(E)):                    #verify conflict-freeness
-            for j in range(i+1, len(E)):
-                if [E[i], E[j]] in this.attacks:
+        for i in E:
+            for j in E:
+                if [i,j] in this.attacks:
                     return False
-        
         val = False
         for a in E:                                #verify defense
             for b in this.args:
@@ -178,15 +131,31 @@ class AF():
         return True
 
     def is_complete(this, E:list) -> bool:
-        for a in E:                                
-            for b in this.args:                         #we check everything that a is attacking
-                if [a,b] in this.attacks:
-                    for set_attack in this.attacks:            #we check everything attacked by b, and so defended by a
-                        if [b,set_attack[1]] in this.attacks:  
-                            if set_attack[1] not in E:         #if that element is not in E, then E is not a CO
-                                return False
-        return True 
+        if not this.is_admissible(E):
+            return False
+        for a in E:                                       #for each a in E, we're looking if a is defended by E
+            a_attacked_by_list = this.attacked_by(a)      #first we look into everything that attacks a
+            for elem in a_attacked_by_list:               #then we check if each attacker is attacked by E
+                if not this.is_defended(this.attacked_by(elem), E):         #we check if defended by E
+                    return False
+        return True
     
+    def attacked_by(this, a) -> list:       #returns the list of everything that attacks a
+        res = []
+        for attack in this.attacks:
+            if attack[1] == a:
+                res.append(attack[0])
+        return set(res)
+
+    def is_defended(this, defensers_list, E) -> bool:  #returns True if at least one defenser is in E 
+        for elem in defensers_list:
+            if elem in E:
+                return True
+        return False 
+
+    
+
+
     def is_stable(this, E:list) -> bool:
         not_E = set(this.args) - set(E)      # A \ E
         
@@ -201,6 +170,60 @@ class AF():
                 val = False
         return True
 
+
+    ## GENERATE ALL CF PART:
+
+    def generate_all_cf(self) -> list:       #generate all cf sets for the AF
+        res = [set([])]
+        for elem in self.args:
+            res_to_add = self.cf(elem)       #calculte all cfs induced by each element of the AF
+            for elem in res_to_add:
+                if isinstance(elem,str):     #if a single element add it as a set, else just append, we do this because we want a list of sets
+                    res.append(set(elem))
+                else:
+                    res.append(elem)
+        return res
+
+    def cf(self, a) -> list:            # returns list of everything a defends, recusrive function
+        if self.elem_defends(a) == []:  
+            return a
+        res = [set(a)]
+        elems_defended_by_a = self.elem_defends(a)
+        for elem in elems_defended_by_a:
+            #print(a, elem, ":", [a] + elem)
+            res.append(set([a] + elem))      #we do this because we want the complete list of args in the set, not just elem but also a
+        return self.cf(res)
+        
+    
+    def elem_defends(self, a) -> list:                  #return list of every args a defends
+        a_attacks = self.elem_attacks(a)
+        a_defends = []
+        for elem in a_attacks:
+            if self.elem_attacks(elem) != []:
+                for elem_defends in flatten(self.elem_attacks(elem)):
+                    if elem_defends != a and elem_defends not in a_attacks:
+                        a_defends.append(self.elem_attacks(elem))
+        #print(a, "attacks:", a_attacks, "and denfends: ", a_defends)
+        return a_defends
+
+    def elem_attacks(self, a) -> list:                  #return list of every args a attacks
+        res = []
+        for attack in self.attacks:
+            if attack[0] == a:
+                res.append(attack[1])
+        return res
+
+
+
+def majuscule(E):                    #uppers all character in E, usefull in main
+        res = []
+        for elem in E:
+            res.append(elem.upper())
+        return res
+
+def flatten(xss):                    #flattens a list of list
+    return [x for xs in xss for x in xs]
+
 def main():
     parser = argparse.ArgumentParser()
     # Add script arguments
@@ -213,22 +236,22 @@ def main():
 
 
     if args.p == "VE-CO":
-        print("YES") if af.VE_CO(args.a.split(",")) else print("NO")
+        print("YES") if af.VE_CO(majuscule(args.a.split(","))) else print("NO")
 
     elif args.p == "DC-CO":
-        print("YES") if af.DC_CO(args.a) else print("NO")
+        print("YES") if af.DC_CO(args.a.upper()) else print("NO")
 
     elif args.p == "DS-CO":
-        print("YES") if af.DS_CO(args.a) else print("NO")
+        print("YES") if af.DS_CO(args.a.upper()) else print("NO")
 
     elif args.p == "VE-ST":
-        print("YES") if af.VE_ST(args.a.split(",")) else print("NO")
+        print("YES") if af.VE_ST(majuscule(args.a.split(","))) else print("NO")
 
     elif args.p == "DC-ST":
-        print("YES") if af.DC_ST(args.a) else print("NO")
+        print("YES") if af.DC_ST(args.a.upper()) else print("NO")
 
     elif args.p == "DS-ST":
-        print("YES") if af.DS_ST(args.a) else print("NO")
+        print("YES") if af.DS_ST(args.a.upper()) else print("NO")
 
 if __name__ == "__main__":
     main()
